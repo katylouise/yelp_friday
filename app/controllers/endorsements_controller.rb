4d9098b023457054dda76557f33edc8e20e1ed34
Rebecca Appleyard <rklappleyard@gmail.com>
2015-09-04 18:43:43 +0100

    AJAX endorsements

--- app/controllers/endorsements_controller.rb
+++ app/controllers/endorsements_controller.rb
@@ -1,12 +1,7 @@
 class EndorsementsController < ApplicationController
-  # def new
-  #   @review = Review.find(params[:review_id])
-  #   @endorsement = Endorsement.new
-  # end
-
   def create
     @review = Review.find(params[:review_id])
     @review.endorsements.create
-    redirect_to restaurants_path
+    render json: {new_endorsement_count: @review.endorsements.count}
   end
 end
